16848ce3b5f2874e72be93cd4acd2f11f3b01fb7
pjz <1002364145@qq.com>
2019-10-10 16:31:22 +0800

    6.0 和 8.1/9.0 RecoveryUI 修改

---
+++ RecoveryUI 横屏修改/8.1+9.0/mt_graphic_rotate.h
@@ -0,0 +1,18 @@
+/*
+* Copyright (C) 2014 MediaTek Inc.
+* Modification based on code covered by the mentioned copyright
+* and/or permission notice(s).
+*/
+
+#ifndef MT_GRAPHICS_ROTATE_H_
+#define MT_GRAPHICS_ROTATE_H_
+
+//no need minui.h
+//#include "minui.h"
+
+void rotate_canvas_exit(void);
+void rotate_canvas_init(GRSurface *gr_draw);
+void rotate_surface(GRSurface *dst, GRSurface *src);
+GRSurface *rotate_canvas_get(GRSurface *gr_draw);
+
+#endif
